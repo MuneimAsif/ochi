5628454e0731147af89990da5f4ed688e88ce60d
muneimasif2004@gmail.com <muneimasif2004@gmail.com>
2024-08-13 23:46:59 -0700

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()]
+  // build:{
+  //   title:'Presentation Design Agency | Presentation Design Services',
+  //   favicon:'./src/assets/ochi.png'
+  // }
 })
